7ee8d1d6a97fa71378659d5188a21cfa1b0b2ef7
bianwenbo <bianwenbo@gmail.com>
2022-06-16 22:19:01 +0800

    v0.2.0版本 自动锁屏功能 - bugfix 设置数值即时生效

--- BreakTimeCS/Properties/Settings.Designer.cs
+++ BreakTimeCS/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BreakTimeCS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,9 +26,9 @@ namespace BreakTimeCS.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("25")]
-        public double LongDuration {
+        public int LongDuration {
             get {
-                return ((double)(this["LongDuration"]));
+                return ((int)(this["LongDuration"]));
             }
             set {
                 this["LongDuration"] = value;
@@ -38,9 +38,9 @@ namespace BreakTimeCS.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("5")]
-        public double ShortDuration {
+        public int ShortDuration {
             get {
-                return ((double)(this["ShortDuration"]));
+                return ((int)(this["ShortDuration"]));
             }
             set {
                 this["ShortDuration"] = value;
--- BreakTimeCS/ViewModels/MainViewModel.cs
+++ BreakTimeCS/ViewModels/MainViewModel.cs
@@ -31,8 +31,6 @@ namespace BreakTimeCS.ViewModels
         private readonly DispatcherTimer _timer = new();
         private DateTime lockTime;
         private DateTime unlockTime;
-        private readonly TimeSpan longDuration = TimeSpan.FromMinutes(Properties.Settings.Default.LongDuration);
-        private readonly TimeSpan shortDuration = TimeSpan.FromMinutes(Properties.Settings.Default.ShortDuration);
         private TimeSpan _countdown;
         public DateTime UnlockTime
         {
@@ -58,19 +56,51 @@ namespace BreakTimeCS.ViewModels
                 }
             }
         }
-        private uint _idleTime;
-        public uint IdleTime
+        public int LongDuration
         {
-            get => this._idleTime;
+            get => Properties.Settings.Default.LongDuration;
             set
             {
-                if (this._idleTime != value)
+                if (Properties.Settings.Default.LongDuration != value)
                 {
-                    this._idleTime = value;
-                    this.NotifyPropertyChanged(nameof(IdleTime));
+                    Properties.Settings.Default.LongDuration = value;
+                    this.NotifyPropertyChanged(nameof(LongDuration));
+                    Properties.Settings.Default.Save();
+                    //计时器重置
+                    if (_timer.IsEnabled == true)
+                    {
+                        Countdown = TimeSpan.FromMinutes(LongDuration);
+                        _timer.Start();
+                    }
                 }
             }
         }
+        public int ShortDuration
+        {
+            get => Properties.Settings.Default.ShortDuration;
+            set
+            {
+                if (!Properties.Settings.Default.ShortDuration.Equals(value))
+                {
+                    Properties.Settings.Default.ShortDuration = value;
+                    this.NotifyPropertyChanged(nameof(ShortDuration));
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+        //private uint _idleTime;
+        //public uint IdleTime
+        //{
+        //    get => this._idleTime;
+        //    set
+        //    {
+        //        if (this._idleTime != value)
+        //        {
+        //            this._idleTime = value;
+        //            this.NotifyPropertyChanged(nameof(IdleTime));
+        //        }
+        //    }
+        //}
         //事件
         void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e) //监听系统锁屏/解锁事件
         {
@@ -86,19 +116,19 @@ namespace BreakTimeCS.ViewModels
         //方法
         void UnlockDo() //解锁干什么
         {
-            TimeSpan gap = DateTime.UtcNow - lockTime;
+            int gap = (int) (DateTime.UtcNow - lockTime).TotalMinutes;
             //解锁间隔时间过短、计时尚未临近结束：继续之前未完的计时
-            if (gap < shortDuration && Countdown > shortDuration) { }
+            if (gap < ShortDuration && Countdown.TotalMinutes > ShortDuration) { }
             //解锁间隔时间果断、计时已经（临近）结束：短时间计时
-            else if (gap < shortDuration && Countdown < shortDuration)
+            else if (gap < ShortDuration && Countdown.TotalMinutes < ShortDuration)
             {
-                Countdown = shortDuration;
+                Countdown = TimeSpan.FromMinutes(ShortDuration);
             }
             //其余情况重新计时：长时间计时
             else
             {
                 UnlockTime = DateTime.UtcNow;
-                Countdown = longDuration;
+                Countdown = TimeSpan.FromMinutes(LongDuration);
             }
             _timer.Start();
         }
@@ -123,9 +153,20 @@ namespace BreakTimeCS.ViewModels
                         .Show();
                     break;
             }
-            IdleTime = GetIdleTime();
+            switch (1000 * ShortDuration - GetIdleTime())
+            {
+                case 0:
+                    LockWorkStation();
+                    break;
+                case 10:
+                    new ToastContentBuilder()
+                        .AddText("系统已闲置超时，即将锁屏！")
+                        .AddText("任意操作即可取消锁屏。")
+                        .Show();
+                    break;
+            }
         }
-        static uint GetIdleTime()
+        static uint GetIdleTime() //系统空闲时间 ms
         {
             LASTINPUTINFO LastUserAction = new();
             LastUserAction.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(LastUserAction);
